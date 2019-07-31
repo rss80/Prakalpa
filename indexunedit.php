accbf1c52d3c529806255509ce295e746ed0883f
Rohit Surana <Rohit.Surana@svclnk.com>
2019-07-31 11:38:26 -0400

    Prakalpa website

---
+++ indexunedit.php
@@ -0,0 +1,146 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en-US" xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
+<head>
+<title>ISTE- Indian Society for Technical Education</title>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<!--<link rel="shortcut icon" href="css/images/favicon.ico" />-->
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+<!--[if IE 6]>
+		<link rel="stylesheet" href="css/ie6.css" type="text/css" media="all" />
+		<script src="js/png-fix.js" type="text/javascript"></script>
+	<![endif]-->
+<script src="js/jquery-1.4.2.js" type="text/javascript"></script>
+<script src="js/jquery.jcarousel.js" type="text/javascript"></script>
+<script src="js/js-func.js" type="text/javascript"></script>
+
+<!--Tracking Code -->
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36509872-1']);
+  _gaq.push(['_setDomainName', 'iste-kjsce.org']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</head>
+<body>
+<!-- Header -->
+<?php include 'header.php';?>
+
+<!-- Slider -->
+<div id="slider">
+  <div class="shell">
+    <div class="slider-holder">
+      <div class="slider-left">
+        <ul>
+		  <li>
+            <h2>Prakalpa 2016</h2>
+            <p>K. J. Somaiya College of Engineering and the ISTE Student Chapter – KJSCE is organizing “PRAKALPA 2016” on 16th , 18 and 19th March, 2015. The competition is being organized since 2006 and is now becoming more and more popular among the students.The theme of this year's Prakalpa is DIGITAL INDIA.
+	</p>
+			<a href="Prakalpa/Prakalpa16.html"><font color="#fff">More.</font></a>
+          </li>
+		  <li>
+            <h2>Seminar on <br>"Basic concepts of OOP"</h2>
+            <p>A seminar on the basic concepts of object-oriented programming using Java programming language. A number of Java example programs were shown in the presentation.</p>
+            <a href="oop.php"><font color="#fff">More.</font></a>
+			</li>
+		  <li>
+            <h2>Abhiyantriki' 14</h2>
+            <p>All the 3 events organised by ISTE Council had a great and overwhelming response in the Abhiyantriki. Good number of crowd participated in the events and enjoyed it to the core.</p>
+            <a href="abhiyantriki14.php"><font color="#fff">More.</font></a> 
+			</li>
+		  <li>
+            <h2>Workshop on AVR ROBOTICS</h2>
+            <p>Students were taught to design a robot using AVR technology. Basic teaching included C & embedded C language.</p>
+            <a href="avr14.php"><font color="#fff">More.</font></a>
+          </li>
+		  <li>
+            <h2>Independence Day Celebration 2014</h2>
+            <p>The History repeates itself. As every year, this year as well Independence Day was celebrated on <b>16<sup>th</sup> Aug</b>. The celebration included National Anthem sung by the entire crowd present at the event. Event commencement and the conclusion was well within time.</p>
+            <a href="iday14.php"><font color="#fff">More.</font></a> 
+		  </li>
+          <li>
+            <h2>Seminar on Blogging</h2>
+            <p>From the very basic concepts of Blogging to the explanation of a way to earn some Money from Blogging was explained. Necessity, Scope, Technical Aspects, Ethics about the Blogging formed the later part of the Seminar.</p>
+            <a href="blogging.php"><font color="#fff">More.</font></a>
+          </li>
+        </ul>
+      </div>
+      <div class="slider-right">
+        <ul>
+		  <li><a href="prakalpa15.php"><img src="prakpics/prakalpa15_poster.png" height="350px" width= "640px" alt="" /></a></li>
+		  <li><img src="Workshops/java workshop.jpg" height="350px" width= "640px" alt="" /></li>
+		  <li><img src="Abhiyantriki/Abhiyantriki-2014/ALIEN.jpg" height="350px" width= "640px" alt="" /></li>
+		  <li><img src="Workshops/AVR14/final.JPG" height="350px" width= "640px" alt="" /></li>
+		  <li><img src="images/Independence Day-2014/IMG_1171.JPG" height="350px" width= "640px" alt="" /></li>
+		  <li><img src="Workshops/Seminar on Blogging/Just the Start.JPG" height="350px" width= "640px" alt="" /></li>
+		  
+          
+        </ul>
+      </div>
+      <div class="cl">&nbsp;</div>
+      <div class="slider-nav"> <a href="#">1</a> <a href="#">2</a> <a href="#">3</a> <a href="#">4</a> <a href="#">5</a> <a href="#">6</a>
+        <div class="cl">&nbsp;</div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- End Slider --> 
+<!-- Main -->
+<div id="main">
+  <div class="shell">
+    <div class="col">
+      <center>
+        <h2><font color="black">Recent</font></h2>
+      </center>
+      <ul>
+	<li><a href="matlab15.php">Abhiyantriki 2015</a></li>  
+	<li><a href="prakalpa15.php">GATE Seminar</a></li>
+	<li><a href="oop.php">Independence Day Celebration-2015</a></li>
+	<li><a href="abhiyantriki14.php">Abhiyantriki'14</a></li>  
+  	<li><a href="avr14.php">Workshop on AVR Robotics</a></li>
+	<li> <a href="iday14.php">Independence Day Celebration-2014</a></li>
+	    
+      </ul>
+    </div>
+    <div class="col">
+      <center>
+        <h2><font color="black">Upcomings</font></h2>
+      </center> 
+	 
+	<li><a href="Prakalpa/Prakalpa16.html">Prakalpa 2016</a></li>  
+	<!-- <ul>
+		<li> <a href="prakalpa15.php">Prakalpa'15 - click here for details</a> </li>
+		</ul>  -->
+      <div class="cl">&nbsp;</div> 
+    </div>
+    <div class="col last">
+      <center>
+        <h2><font color="black">News Flash</font></h2>
+      </center>
+      <ul>
+	    <li><a href="/docs/Financial Assistance2014.docx" target='_blank'>Click here</a> to Download Student Development form 2014-15. <br/><a target='_blank' href="/docs/ISTE Student Development Policy 2014.jpg">View Student Development Policy</a></li>
+        <li>
+          <p>Students having any queries are requested to mail us on <a href="mailto:iste.kjsce@gmail.com"><i>iste.kjsce@gmail.com</i></a></p>
+        </li>
+      </ul>
+      <div class="cl">&nbsp;</div>
+      <!--<a href="#" class="find-more">Find out more</a>--> 
+    </div>
+    <div class="cl">&nbsp;</div>
+  </div>
+</div>
+<!-- End Main --> 
+<!-- Footer -->
+<?php include 'footer.php' ?>
+<!-- Footer -->
+</body>
+</html>
